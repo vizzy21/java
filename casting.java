d7620b0e771bb961f6f44ea47ec63886ee43b49f
vizzy21 <85496036+vizzy21@users.noreply.github.com>
2022-09-17 19:02:19 +0530

    casting

---
+++ casting.java
@@ -0,0 +1,13 @@
+public class casting
+{
+	public static void main(String[] args) {
+	//widening casting small to big datatype
+	int x = 20;
+	double y = x;
+	System.out.println(y);
+	//Narrow casting big to small datatype
+	double z = 30.2d;
+	int w = (int)z;
+	System.out.println(w);
+	}
+}
