7dddeef46c1019e5fc36505fe8545e6cc5158832
vizzy21 <85496036+vizzy21@users.noreply.github.com>
2022-09-18 20:37:20 +0530

    Update Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -17,7 +17,7 @@ static void pattern1(int n)
         for (int row = 1; row <= n; row++) 
         {
             // for every row, run the col
-            for (int col = 1; col <= row; col++)
+            for (int col = 1; col <= row; col++)  //n-row +1 for upside down for the same problem
             {
                 System.out.print("* ");
             }
